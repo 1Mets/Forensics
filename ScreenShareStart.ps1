2379dd8b16483c438cf8250c5ca219e8d0878494
1Mets <jaimemichael006@gmail.com>
2026-09-11 17:11:23 -0400

    Update ScreenShareStart.ps1

--- ScreenShareStart.ps1
+++ ScreenShareStart.ps1
@@ -8,6 +8,7 @@ function Start-PersistentScript {
 Start-PersistentScript "https://raw.githubusercontent.com/1Mets/Forensics/refs/heads/main/Start.ps1"
 Start-PersistentScript "https://raw.githubusercontent.com/MeowTonynoh/Tonynoh-PS1/main/JVM-Inspector.ps1"
 Start-PersistentScript "https://raw.githubusercontent.com/MeowTonynoh/MeowModAnalyzer/main/MeowModAnalyzer.ps1"
+Start-PersistentScript "https://raw.githubusercontent.com/p1aegg/powershell/refs/heads/main/ClassLoaderDump.ps1"
 
 iwr -Uri "https://github.com/1Mets/Forensics/raw/refs/heads/main/downloader.exe" -OutFile "$env:USERPROFILE\Downloads\ToolDownloader.exe"
 
