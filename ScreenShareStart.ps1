787ea64f6b2b5e35464814edcbf8db4b9e5ec943
1Mets <jaimemichael006@gmail.com>
2026-09-11 17:03:08 -0400

    Create ScreenShareStart.ps1

---
+++ ScreenShareStart.ps1
@@ -0,0 +1,14 @@
+function Start-PersistentScript {
+    param ([string]$Url)
+    
+    $command = "powershell -NoExit -ExecutionPolicy Bypass -Command `"iex (irm '$Url')`""
+    Start-Process cmd.exe -Verb RunAs -ArgumentList "/k $command"
+}
+
+Start-PersistentScript "https://raw.githubusercontent.com/1Mets/tool-downloader/refs/heads/main/ScreenshareStart.ps1"
+Start-PersistentScript "https://raw.githubusercontent.com/MeowTonynoh/Tonynoh-PS1/main/JVM-Inspector.ps1"
+Start-PersistentScript "https://raw.githubusercontent.com/MeowTonynoh/MeowModAnalyzer/main/MeowModAnalyzer.ps1"
+
+iwr -Uri "https://github.com/1Mets/Forensics/raw/refs/heads/main/downloader.exe" -OutFile "$env:USERPROFILE\Downloads\ToolDownloader.exe"
+
+Start "$env:USERPROFILE\Downloads\ToolDownloader.exe"
